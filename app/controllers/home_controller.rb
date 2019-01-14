ac33eeb96eaa9cdf244ceedf662afa05f8cdf2a3
Brentwood Reid <bwoodreid@gmail.com>
2019-01-14 14:30:12 -0800

    Revert deliveries method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,10 @@ class HomeController < ApplicationController
   include SkipAuthorization
 
   def index; 
-    @leads = current_user.deliveries.order(start_date: :asc)
-    @units = current_user.units
-    @deliveries = @leads + @units.sort_by(&:start_date)
+    @deliveries = current_user.deliveries.order(start_date: :asc)
+    # @leads = current_user.deliveries.order(start_date: :asc)
+    # @units = current_user.units
+    # @deliveries = @leads + @units.sort_by(&:start_date)
   end
 
 end
