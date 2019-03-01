f01af23519357c1a389186800d01b98337de5dbc
Brentwood Reid <bwoodreid@gmail.com>
2019-03-01 09:30:45 -0800

    Display only upcoming deliveries on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,9 @@
 class HomeController < ApplicationController
   include SkipAuthorization
 
-  def index; 
-    @leads = current_user.deliveries.order(start_date: :asc)
+  def index;
+    # @leads = current_user.deliveries.where(:end_date < Date.today).order(start_date: :asc)
+    @leads = current_user.deliveries.where("end_date < ?", Date.today ).order(start_date: :asc)
     @units = current_user.units
     @deliveries = @leads + @units
   end
