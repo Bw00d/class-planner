a8dda56050e031466664382439e394df8ef798d9
Brentwood Reid <bwoodreid@gmail.com>
2021-01-29 10:39:31 -0800

    Add calendar colors

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -9,6 +9,34 @@ $(document).on("turbolinks:load", function() {
             //event.color = "#FFB347"; //Em andamento
             element.css('background-color', '#474747');
             element.css('border', '#474747');
+          } else if (event.number == "ICS-400") {
+            //event.color = "#77DD77"; //Concluído OK
+            element.css('background-color', '#d9e1f1');
+            element.css('border', '#d9e1f1');
+          } else if (event.number == "ICS-300") {
+            //event.color = "#77DD77"; //Concluído OK
+            element.css('background-color', '#a9ce90');
+            element.css('border', '#a9ce90');
+          } else if (event.number == "S-270") {
+            //event.color = "#77DD77"; //Concluído OK
+            element.css('background-color', '#f5ded1');
+            element.css('border', '#f5ded1');
+          } else if (event.number == "S-131") {
+            //event.color = "#77DD77"; //Concluído OK
+            element.css('background-color', '#d7dfef');
+            element.css('border', '#d7dfef');
+          } else if (event.number == "S-200") {
+            //event.color = "#77DD77"; //Concluído OK
+            element.css('background-color', '#fee59d');
+            element.css('border', '#fee59d');
+            } else if (event.number == "S-330") {
+            //event.color = "#77DD77"; //Concluído OK
+            element.css('background-color', '#FD9727');
+            element.css('border', '#FD9727');
+          } else if (event.number == "S-130") {
+            //event.color = "#77DD77"; //Concluído OK
+            element.css('background-color', '#8faad9');
+            element.css('border', '#8faad9');
         } else if (event.number == "S-330") {
             //event.color = "#77DD77"; //Concluído OK
             element.css('background-color', '#FD9727');
