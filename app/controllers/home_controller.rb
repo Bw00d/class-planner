6e214a648c364d8c87fec0fcf5461b2d64a09d3e
Brentwood Reid <bwoodreid@gmail.com>
2021-04-20 10:59:58 -0700

    Test envs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@ class HomeController < ApplicationController
   include SkipAuthorization
 
   def index;
+    @username = "SENDGRID_USERNAME: #{ENV['SENDGRID_USERNAME']}"
+    @password =  "SENDGRID_PASSWORD: #{ENV['SENDGRID_PASSWORD']}"
     @leads = current_user.deliveries.where('extract(year  from start_date) = ?', 2021).order(start_date: :asc)
     @expired_leads = current_user.deliveries.where('extract(year  from start_date) = ?', 2020).order(start_date: :asc)
     @units = current_user.units
