afa6cc876bce7514920ca214fb499e82717137eb
Brentwood Reid <bwoodreid@gmail.com>
2021-04-20 09:55:46 -0700

    Add mailing functionality

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,16 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'https://ccc-planner.herokuapp.com' }
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: 587,
+    domain: Rails.application.secrets.domain_name,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password
+  }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
