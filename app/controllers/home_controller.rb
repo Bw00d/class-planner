b059e27f7fc1855a97fa0de1bec4d0febd6774f6
Brentwood Reid <bwoodreid@gmail.com>
2021-12-14 08:46:36 -0800

    Update @leads and @units with current year

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,8 +5,8 @@ class HomeController < ApplicationController
   def index;
     @username = "SENDGRID_USERNAME: #{ENV['SENDGRID_USERNAME']}"
     @password =  "SENDGRID_PASSWORD: #{ENV['SENDGRID_PASSWORD']}"
-    @leads = current_user.deliveries.where('extract(year  from start_date) = ?', 2021).order(start_date: :asc)
-    @expired_leads = current_user.deliveries.where('extract(year  from start_date) = ?', 2020).order(start_date: :asc)
+    @leads = current_user.deliveries.where('extract(year  from start_date) = ?', 2022).order(start_date: :asc)
+    @expired_leads = current_user.deliveries.where('extract(year  from start_date) = ?', 2021).order(start_date: :asc)
     @units = current_user.units
     @deliveries = @leads + @units
   end
